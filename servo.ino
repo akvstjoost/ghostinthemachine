dbdb00db2b455df35401af1bccda8c1196f04e1f
ThomasR <thomasrutgers@gmail.com>
2022-02-17 10:30:00 +0100

    Update servo.ino

--- servo.ino
+++ servo.ino
@@ -3,45 +3,71 @@
 #include <Servo.h>
 Servo servo1;
 Servo servo2;
-int _pos1 = 0;
-int _pos2 = 0;
+int servosLastPos = [0,0];
+int motorsLastPWM = [0,0];
+int motorPins = [9,10,11,12];
 
 void setup() {
   servo1.attach(3);
   servo2.attach(4);
-  pinMode(11, OUTPUT);
-  pinMode(12, INPUT_PULLUP);
-  digitalWrite(11, LOW);
+  pinMode(5, OUTPUT);
+  pinMode(6, INPUT_PULLUP);
+  digitalWrite(5, LOW);
+  pinMode(8,OUTPUT);
+  pinMode(9,OUTPUT);
+  pinMode(10,OUTPUT);
+  pinMode(11,OUTPUT);
+  pinMode(12,OUTPUT);
+  pinMode(13,OUTPUT);
 }
 
 void loop() {
   //schrijf hier je code
   moveServos(0, 0, 1000);
   moveServos(180, 180, 1000);
-
+  accelMotor(2, 0, 100);
 }
 
 
 
-void moveServos(int pos1, int pos2, int time) {
+void moveServos(int servo1NewPos, int servo2NewPos, int time) { 
   int loops = time / 20;
   if (loops < 1) loops = 1;
-  int start1 = _pos1;
-  int start2 = _pos2;
-  int stop1 = pos1;
-  int stop2 = pos2;
   for (int l = 0; l < loops; l++) {
-    servo1.write(calcPos(l, loops, start1, stop1));
-    servo2.write(calcPos(l, loops, start2, stop2));
+    servo1.write(calcPos(l, loops, servosLastPos[0], servo1NewPos));
+    servo2.write(calcPos(l, loops, servosLastPos[1], servo2NewPos));
     delay(20);
     do {
-    } while (digitalRead(12) == LOW);
+    } while (digitalRead(6) == LOW);
 
   }
-  _pos1 = pos1;
-  _pos2 = pos2;
+  servosLastPos[0] = servo1NewPos;
+  servosLastPos[1] = servo2NewPos;
+}
+
+void accelMotor(int motorNr, float percentage, int time) {
+  int motorIndex = motorNr - 1 ;
+  int motorNewPWM = constrain(precentage * 2.55,-255,255);  
+  int loops = time / 20;  if (loops < 1) loops = 1;
+  for (int l = 0; l < loops; l++) {
+    
+    if(speed>0) {
+      digitalWrite(motorPin[motorIndex], LOW);
+      analogWrite(motorPin[motorIndex+1], calcPos(l, loops, motorLastPWM[motorIndex], motorNewPWM));
+    } else {
+      digitalWrite(motorPin[motorIndex+1], LOW);
+      analogWrite(motorPin[motorIndex], calcPos(l, loops, motorLastPWM[motorIndex], motorNewPWM));      
+    }
+    delay(20);
+    do {
+    } while (digitalRead(6) == LOW);
+
+  }
+  motorLastPWM[motorIndex] = motorNewPWM;
 }
 
 int calcPos(float loop, float loops, float start, float end) {
   return int(end * (loop / loops)) + (start * (1 - (loop / loops)));
 }
+
+
