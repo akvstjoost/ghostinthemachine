08fddb8c9b41dbca67d958e5839b8e1f141920e5
ThomasR <thomasrutgers@gmail.com>
2022-02-17 12:50:55 +0100

    Update simpeldc.ino

--- simpeldc.ino
+++ simpeldc.ino
@@ -11,12 +11,15 @@ void loop() {
   digitalWrite(9,HIGH);
   digitalWrite(10,LOW);
   delay(1000);
+  
   digitalWrite(9,LOW);
   digitalWrite(10,LOW);
   delay(1000);
+  
   digitalWrite(9,LOW);
   digitalWrite(10,HIGH);
   delay(1000);
+  
   digitalWrite(9,LOW);
   digitalWrite(10,LOW);
   delay(1000);
